bd26283901354f41c5b0d10c8f47e293160dd852
Darrnel <redzsiby14@gmail.com>
2018-12-06 21:23:24 +0100

    Layoutok elkészítése + ezekhez az Activity: - egyéni - sablon - main

--- app/src/main/java/com/example/uzt/edzsterv/MainActivity.java
+++ app/src/main/java/com/example/uzt/edzsterv/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.uzt.edzsterv;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.uzt.edzsterv.R;
+import com.example.uzt.edzsterv.activities.EdzestervMainActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +19,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        configuregoToEdzestervButton();
+
+    }
+
+    private void configuregoToEdzestervButton(){
+        Button goToEdzesterButton = (Button) findViewById(R.id.goToEdzestervButton);
+
+        goToEdzesterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EdzestervMainActivity.class));
+            }
+        });
+    }
+    public void goToEdzesterv(View view) {
+    }
+
+    public void goToEtrend(View view) {
     }
 }
---
+++ app/src/main/java/com/example/uzt/edzsterv/activities/EdzestervEgyeniActivity.java
@@ -0,0 +1,13 @@
+package com.example.uzt.edzsterv.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EdzestervEgyeniActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edzesterv_egyedi);
+    }
+}
